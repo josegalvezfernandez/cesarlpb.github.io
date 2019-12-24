456093380e3992dd88bcf2f640ca78604ad4fc30
cesarlpb <cesarlpb@gmail.com>
2019-12-24 12:50:41 +0100

    updates
    updates for English lessons

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
-var cor_ans = ["am", "'re", "are"];
-var ans = ["0", "0", "0"];
+var cor_ans = ["am", "'re", "are", "are not", "aren't"];  
+var ans = ["0", "0", "0", "0", "0"];
 // Submit form with name function.
 function submit_by_name() {
 	var i;
 	var score = 0;
 	//console.log(ans[0]);
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 5; i++) {
 		var str = String(i+1);
 		//console.log("q"+str);
 		ans[i] = document.getElementById("q"+str).value;
 		//console.log(ans[i]);
 	}
 
-	for (i = 0; i < 3; i++){
+	for (i = 0; i < 5; i++){
 		if (ans[i] == cor_ans[i]) {
+			//console.log(ans[i]);
 			score++;
 		}
 	}
@@ -28,12 +29,16 @@ function submit_by_name() {
 		+ "\n\n1."+ ans[0] 
 		+ "\n2."+ ans[1]
 		+ "\n3."+ ans[2]
-		+ "\n\nScore: " + score);
+		+ "\n4."+ ans[3]
+		+ "\n5."+ ans[4]
+		+ "\n\nScore: " + score + "/ 5");
 	}
 //console.log(ans[1]);
 function show_answers() {
 	alert("Correct Answers:"
 		+ "\n\n1. " + cor_ans[0]
 		+ "\n2. " + cor_ans[1]
-		+ "\n3. " + cor_ans[2]);
+		+ "\n3. " + cor_ans[2]
+		+ "\n4. " + cor_ans[3]
+		+ "\n5. " + cor_ans[4]);
 }
