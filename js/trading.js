c36346a62afdba99de63291373dfb8e692c15417
cesarlpb <cesarlpb@gmail.com>
2020-02-20 20:52:27 +0100

    Adding Trading Samples
    Bitcoin prices graphs

---
+++ js/trading.js
@@ -0,0 +1,38 @@
+AmCharts.loadFile( "https://cesarlpb.github.io/files/samples.csv", {}, function( response ) {
+
+  /**
+   * Parse CSV
+   */
+  var data = AmCharts.parseCSV( response, {
+    "useColumnNames": true
+  } );
+  
+  
+  // Data loader
+  // Create the table
+  var table = document.createElement("table");
+  
+  // Add headers
+  var row = document.createElement("tr");
+  for(var key in data[0]) {
+    var col = document.createElement("th");
+    col.innerHTML = key;
+    row.appendChild(col);
+  }
+  table.appendChild(row);
+  
+  // Add rows
+  for(var i = 0; i < data.length; i++) {
+    var row = document.createElement("tr");
+    for(var key in data[i]) {
+      var col = document.createElement("td");
+      col.innerHTML = data[i][key];
+      row.appendChild(col);
+    }
+    table.appendChild(row);
+  }
+  
+  // Add table to document
+  document.getElementById("datatable").appendChild(table);
+
+} );
