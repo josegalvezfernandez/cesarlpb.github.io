5303c35c68f7b38b3cadcdc1353a744e52c2b2fa
cesarlpb <cesarlpb@gmail.com>
2020-05-13 15:46:28 +0200

    updates
    Python scripts para conectar con web, scrap y API

---
+++ Python/Api2/api2.py
@@ -0,0 +1,27 @@
+# API
+#Apikey: zv3u0zzhv10ojbnnrzm1rhc4rnxpjcv5
+#Secret: L4z1zpuu1LKH
+#Access is free to a maximum of 100 req/month and 
+#it’s limited by 1 req/sec. If you need to perform a greater number of requests, 
+#let us know.
+def get_oauth_token():
+	http_obj = Http()
+	url = "https://api.idealista.com/oauth/token"
+	apikey= urllib.parse.quote_plus('zv3u0zzhv10ojbnnrzm1rhc4rnxpjcv5')
+	secret= urllib.parse.quote_plus('L4z1zpuu1LKH')
+	auth = base64.encode(apikey + ':' + secret)
+	body = {'grant_type':'client_credentials'}
+	headers = {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8','Authorization' : 'Basic ' + auth}
+	resp, content = http_obj.request(url,method='POST',headers=headers, body=urllib.parse.urlencode(body))
+	#Escribie en TXT
+#	import json 
+#	with open(content, 'w') as json_file:
+#    json.dump("session_token.txt", json_file)
+	text_file = open("session_token.txt", "w")
+	text_file.write(content.text)
+	text_file.close()
+	#if content:
+	#	print("session_token.txt guardado")
+	#else:
+	#	print("No se ha podido conectar")
+	return content
